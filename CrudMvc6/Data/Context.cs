d6911195d1996d8784a653629d2a713ca81cfc6b
Cihan Mert PEKER <c.mertpeker@hotmail.com>
2023-11-07 11:09:44 +0300

    Add project files.

---
+++ CrudMvc6/Data/Context.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CrudMvc6.Data
+{
+    public class Context : DbContext
+    {
+
+        public Context(DbContextOptions options) : base(options) 
+        {
+           
+        }
+       public DbSet<Product> products { get; set; }
+    }
+}
---
+++ CrudMvc6/Data/Product.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CrudMvc6.Data
+{
+    public class Product
+    {
+        [Key]
+        public int Id { get; set; }
+        public string ProductName { get; set; }
+        public string Quantity { get; set; }
+
+    }
+}
